4a955f64a87fa01be50efe181799b6b07613b8ae
Yixin-micro <yixin875+microsoft@gmail.com>
2024-06-11 14:38:39 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember(){
+    var member = {
+        name: "Kusuma",
+        age: 30,
+        skills: ["JavaScript", "ReactJS", "NodeJS"],
+        address: {
+            street: "Jl. Mangga",
+            city: "Jakarta"
+        }
+    }
+
+    return member;
+
+}
